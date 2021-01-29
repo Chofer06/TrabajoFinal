2f97f5f39f09026502969432039b229502ab87ba
Yina Quiroz <quirozyina@gmail.com>
2021-01-29 12:29:30 -0500

    cambio Yina

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     HOLA COMO ESTAS YINA
+                    Hola Cara de Polla
                 </div>
             </div>
         </div>
